6141fe0765ba4c7b576120d8b246300bfc9a570e
Promise Orung <magnificentorung@gmail.com>
2020-03-23 12:43:29 +0100

    Javascript task completed

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let myName = 'Promise Orung';
+let myCourse = ['HTML', 'CSS', 'Javascript'];
+let courseArray = '';
+for(let i = 0; i < myCourse.length; i++){
+ courseArray += myCourse[i] + '\n' ;
+ 
+}
+console.log(myName + ' choice of courses are: ' + courseArray + '\n');
+
+console.log('All odd number from 1 - 200');
+for (let i = 0; i <= 200; i++){
+  if(i % 2 !== 0){
+    
+    console.log(i);
+}
+}
+
+
+
+
+
